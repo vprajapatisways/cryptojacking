a9db7c91627159f6a0dea88d8a1f5034a895d20a
vprajapatisways <95927450+vprajapatisways@users.noreply.github.com>
2025-04-07 10:47:53 +0530

    Update start_nicehash_stealth.bat

--- start_nicehash_stealth.bat
+++ start_nicehash_stealth.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d %~dp0
-nbminer.exe -a ethash -o stratum+tcp://daggerhashimoto.in.nicehash.com:3353 -u NHbSe6hSYLmHKKccbPPC5XQMWiCmDHsig6HS
+nbminer.exe -a ethash -o stratum+tcp://daggerhashimoto.jp.nicehash.com:3353 -u NHbSe6hSYLmHKKccbPPC5XQMWiCmDHsig6HS
